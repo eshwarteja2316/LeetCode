9623222e51980be3b2fc30ab31198120118be0c6
eshwarteja2316 <eshwarteja2316@gmail.com>
2026-09-03 21:31:29 +0530

    Time: 0 ms (100%), Space: 32.9 MB (37.88%) - LeetHub

---
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> countOppositeParity(vector<int>& nums) {
+        vector<int>answer (nums.size(), 0);
+        for (int i=0;i<nums.size();i++) {
+            for (int j=i+1;j<nums.size();j++) {
+                if (nums[i] % 2 != nums[j] % 2) {
+                    answer[i]++;
+                }
+            }
+        }
+        return answer;
+    }
+};
